98068fdca7fac3a04672d5f1f110aa00f1e62f67
Iman <imankhorshidi72@gmail.com>
2022-07-05 20:19:19 +0430

    Fix: Navbar Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import Navbar from "./components/Navbar";
+import { Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
@@ -17,12 +18,21 @@ const GlobalStyles = createGlobalStyle`
     font-family: Nunito,-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
+    user-select: none;
   }
-
+  
   *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  a{
+    text-decoration: none;
+  }
+
+  li{
+    list-style: none;
   }
 
 `;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,60 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { NavbarContainer } from "./NavbarContainer";
+import { FaTypo3, FaBars, FaTimes } from "react-icons/fa";
+
 const Navbar = () => {
-  return <nav></nav>;
+  const [openMenu, setOpenMenu] = useState(false);
+  return (
+    <NavbarContainer>
+      <div>
+        <Link to="/" className="logo">
+          TRVL <FaTypo3 />
+        </Link>
+        <div className="menu-icon" onClick={() => setOpenMenu(!openMenu)}>
+          {openMenu ? <FaTimes /> : <FaBars />}
+        </div>
+        <ul className={openMenu ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link
+              to="/"
+              className="nav-links"
+              onClick={() => setOpenMenu(false)}
+            >
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/services"
+              className="nav-links"
+              onClick={() => setOpenMenu(false)}
+            >
+              Services
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/products"
+              className="nav-links"
+              onClick={() => setOpenMenu(false)}
+            >
+              Products
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/sign-up"
+              className="nav-links-mobile"
+              onClick={() => setOpenMenu(false)}
+            >
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </NavbarContainer>
+  );
 };
 
 export default Navbar;
