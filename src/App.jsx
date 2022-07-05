3eb189ee0f6d74b15bf33a84b78e1ef598090660
Iman <imankhorshidi72@gmail.com>
2022-07-05 18:42:52 +0430

    Add: Nunito font and GlobalStyles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <GlobalStyles />
+      <Navbar />
+    </>
+  );
 };
 
 export default App;
+
+const GlobalStyles = createGlobalStyle`
+  body{
+    font-family: Nunito,-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  }
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+`;
