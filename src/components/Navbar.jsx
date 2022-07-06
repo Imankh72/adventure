4d661ea80b92807c13ea07c6f63d4b2019c5649a
Iman <imankhorshidi72@gmail.com>
2022-07-06 10:50:49 +0430

    Fix: Navbar Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,49 +5,42 @@ import { FaTypo3, FaBars, FaTimes } from "react-icons/fa";
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
   return (
-    <nav className="bg-neutral-900  w-[100%] h-[60px] fixed top-0 left-0 text-white text-2xl px-2 z-50  md:flex md:justify-between md:max-w-7xl xl:left-[50%] xl:translate-x-[-50%] ">
+    <nav className="navbar">
       <div className="flex items-center justify-between text-3xl h-full md:gap-3">
         <Link to="/" className=" flex items-center gap-1">
           TRVL <FaTypo3 />
         </Link>
         {openMenu ? (
           <FaTimes
-            className="w-8 hover:cursor-pointer hover:opacity-80 transition-all md:hidden"
+            className="mobile-menu-icon"
             onClick={() => setOpenMenu(false)}
           />
         ) : (
           <FaBars
-            className="w-8 hover:cursor-pointer hover:opacity-80 transition-all md:hidden"
+            className="mobile-menu-icon"
             onClick={() => setOpenMenu(true)}
           />
         )}
       </div>
       <ul
-        className={`flex flex-col absolute w-full h-[100vh] bg-neutral-900 py-5 ${
+        className={`nav-link-container ${
           openMenu ? "left-0" : "left-[-100%]"
-        } transition-all duration-700 ease-in-out md:flex-row md:w-auto md:h-auto md:static md:space-y-0 md:items-center md:justify-center`}
+        } `}
       >
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            isActive ? "nav-link active" : "nav-link"
-          }
-        >
+        <NavLink to="/" className="nav-link" onClick={() => setOpenMenu(false)}>
           Home
         </NavLink>
         <NavLink
           to="/services"
-          className={({ isActive }) =>
-            isActive ? "nav-link active" : "nav-link"
-          }
+          className="nav-link"
+          onClick={() => setOpenMenu(false)}
         >
           Services
         </NavLink>
         <NavLink
           to="/products"
-          className={({ isActive }) =>
-            isActive ? "nav-link active" : "nav-link"
-          }
+          className="nav-link"
+          onClick={() => setOpenMenu(false)}
         >
           Products
         </NavLink>
