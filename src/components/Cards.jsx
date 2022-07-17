42577f8797aeb4125bad6347e6ea15a14379ae2b
Iman <imankhorshidi72@gmail.com>
2022-07-17 12:47:16 +0430

    Add: Cards Component

---
+++ src/components/Cards.jsx
@@ -0,0 +1,12 @@
+const Cards = () => {
+  return (
+    <div>
+      <h1>Check out these EPIC destinations!</h1>
+      <div>
+        <ul></ul>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Cards from "../components/Cards";
 import HeroSection from "../components/HeroSection";
 
 const Home = () => {
   return (
     <div className="mt-[80px]">
       <HeroSection />
+      <Cards />
     </div>
   );
 };
