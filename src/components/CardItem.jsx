09c31b24935dad49e7ea8cc8fc425b9ef74d2ef8
Iman <imankhorshidi72@gmail.com>
2022-07-17 20:22:43 +0430

    Fix: Cards and CardItem Component

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -4,16 +4,22 @@ const CardItem = ({ adventure }) => {
   const { text, image, label } = adventure;
 
   return (
-    <li>
+    <li className="rounded-lg w-[280px] h-[320px] overflow-hidden shadow-lg md:w-[440px] md:h-[400px]">
       <Link to="/services">
-        <figure>
-          <img src={image} alt={text} />
-        </figure>
-        <div>
-          <h5>{text}</h5>
+        <div className="w-full h-[180px] relative overflow-hidden md:h-[280px]">
+          <img
+            className="w-full h-full transition-all duration-1000 hover:scale-125"
+            src={image}
+            alt={text}
+          />
+          <div className="p-4">
+            <span className="bg-blue-600 absolute bottom-1 left-1 rounded p-1 text-gray-50">
+              {label}
+            </span>
+          </div>
         </div>
-        <div>
-          <span>{label}</span>
+        <div className="p-4 text-lg font-semibold">
+          <h5>{text}</h5>
         </div>
       </Link>
     </li>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -10,15 +10,17 @@ const Cards = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Check out these EPIC destinations!</h1>
+    <div className="p-4">
+      <h1 className="text-center text-3xl font-bold my-12 md:text-5xl">
+        Check out these EPIC destinations!
+      </h1>
       <div>
-        {adventures.length > 0 &&
-          adventures.map((adventure) => (
-            <ul key={adventure.id}>
-              <CardItem adventure={adventure} />
-            </ul>
-          ))}
+        <ul className="flex flex-col items-center justify-center  gap-8 sm:flex-row sm:flex-wrap">
+          {adventures.length > 0 &&
+            adventures.map((adventure) => (
+              <CardItem key={adventure.id} adventure={adventure} />
+            ))}
+        </ul>
       </div>
     </div>
   );
