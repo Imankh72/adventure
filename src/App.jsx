5967563868751297e655e8c997f8909f6a918345
Iman <imankhorshidi72@gmail.com>
2022-07-18 11:15:07 +0430

    Add: Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Services from "./pages/Services";
 
 const App = () => {
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="max-w-7xl mx-auto p-4">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/services" element={<Services />} />
       </Routes>
+      <Footer />
     </div>
   );
 };
